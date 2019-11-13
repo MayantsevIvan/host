94ff7c65781e265aade6908a98ee6b04c259a4e9
MayantsevIvan <31386901+MayantsevIvan@users.noreply.github.com>
2019-11-13 16:13:29 +0300

    Add files via upload

--- game/JLines.js
+++ game/JLines.js
@@ -449,6 +449,9 @@
 			let self = this;
 			if (this._countSteps <= 0) return;
 			if (this._animation2) return;
+			console.log(evt.data.global.x,evt.data.global.y);
+			console.log(this.gemsContainer.x,this.gemsContainer.y);
+			
 			
 			let gx = Math.floor( evt.data.global.x / pixiApp.stage.scale.x );
 			//let gx = Math.floor( evt.data.global.x / pixiApp.stage.scale.x );
@@ -458,10 +461,14 @@
 			let tx = gx - Handler.gemsContainerGlobalX - Math.floor(Consts.coordsWidth/2);
 			let ty = gy - Handler.gemsContainerGlobalY - Math.floor(Consts.coordsHeight/2);
 			
+			
 	//		let kittenSize = 60*Handler.scaleOfGemsContainer;
 			let fi = Math.ceil( tx / Consts.coordsWidth );//gorizontal number of cat on game field
 			let fj = Math.ceil( ty / Consts.coordsHeight );//vertical   number of cat on game field
-			console.log( fi,fj, evt.data.global.x, evt.data.global.y );
+			console.log( gx, gy );
+			console.log( tx, ty );
+			console.log( fi, fj );
+			return;
 			let cbox = this.getBox(fi,fj);
 			if ( cbox.color == Consts.STONE_COLOR ) return;
 			if ( cbox.color == Consts.IRON_BOX ) return;
