22aeec6db616e476c66df7a6285863905e3bcf46
MayantsevIvan <31386901+MayantsevIvan@users.noreply.github.com>
2019-11-13 14:28:02 +0300

    Add files via upload

--- winds/CWindGame.js
+++ winds/CWindGame.js
@@ -204,8 +204,9 @@
 		Handler.jlines.gemsContainer.x = Consts.coordsShiftX;
 		Handler.jlines.gemsContainer.y = Consts.coordsShiftY;
 		
+		console.log("VERSION 1.6");
 		let shiftXgame = isMobile ? self._sprite.width/2 - 32 : self._sprite.width/2  - 58;
-		let shiftYgame = isMobile ? self._sprite.width/2 - 10 : self._sprite.height/2 - 20;
+		let shiftYgame = isMobile ? self._sprite.width/2 - 40 : self._sprite.height/2 - 20;
 		
 //		Handler.gemsContainerGlobalX = self.mainGroup.x + Consts.coordsShiftX;
 //		Handler.gemsContainerGlobalY = self.mainGroup.y + Consts.coordsShiftY;
