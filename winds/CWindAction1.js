8e573c0982aa456cb1be7a7a6aa232667a959a7c
MayantsevIvan <31386901+MayantsevIvan@users.noreply.github.com>
2019-12-06 16:14:25 +0300

    Add files via upload

--- winds/CWindAction1.js
+++ winds/CWindAction1.js
@@ -1,64 +1,124 @@
 	
 	"use strict";
 	
-	let CWindAcSale1 = {};
+	let CWindAction1 = {};
 	
-	CWindAcSale1.startup = function( params ) {
+	CWindAction1.newObject = function() {
+		this.wind = {};
+		return this;
+	};
+	
+	CWindAction1.startup = function( params ) {
 		let self = this;
 		self.mainGroup = Handler.newGroup();
 		self.mainGroup.x = Handler.contentCenterX;
 		self.mainGroup.y = Handler.contentCenterY;
 		let showContent = function() {
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "backgrAcSale1.png",0,0,641,366);
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "lableAcSale1.png",0,-110,580,75);
-			let but = Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "butBuyAcSale1.png",-80,125,281,69);
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "lableBonAcSale1.png",-90,10,354,142);
-			let cross = Handler.showImgRect(self.mainGroup, "cross.png",300,-160,36,36);
+			//backgr
+			let wBackgr = isMobile ? 454 : 641;
+			let hBackgr = isMobile ? 642 : 366;
+			let nameBackgr = isMobile ? "backgrAcSale1Mob.png" : "backgrAcSale1.png";
+			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + nameBackgr,0,0,wBackgr,hBackgr);
+			//cross
+			let xCross = isMobile ?  202 :  300;
+			let yCross = isMobile ? -296 : -160;
+			let cross = Handler.showImgRect(self.mainGroup, "cross.png", xCross, yCross,36,36);
 			let onCross = function(evt) {
 				self.shutdown();
 			}
 			cross.on("pointertap",onCross);
-			let onBut = function(evt) {
-
-			};
-			but.on("pointertap", onBut);
-		
-			let backgrAnim = Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "backgrAnimAcSale1.png",185,35,164,205);
-			let luch1 = Handler.showImgRect(self.mainGroup, "luch.png",185,35,200,200);
-			luch1.alpha = 0.7;
-			let luch2 = Handler.showImgRect(self.mainGroup, "luch.png",185,35,200,200);
-			luch2.alpha = 0.7;
+			//title
+			let nameTitle = isMobile ? "lableAcSale1Mob.png" : "lableAcSale1.png";
+			let wTitle = isMobile ?  418 :  580; 
+			let hTitle = isMobile ?  132 :   75;
+			let yTitle = isMobile ? -205 : -100;
+			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + nameTitle,0, yTitle, wTitle, hTitle);
+			//bonus
+			let nameBackgrBon = isMobile ? "backgrAnimAcSale1Mob.png" : "backgrAnimAcSale1.png";
+ 			let wBackgrBon = isMobile ? 193 : 164;
+			let hBackgrBon = isMobile ? 240 : 205;
+			let xBackgrBon = isMobile ? -12 : 185;
+			let yBackgrBon = isMobile ? -20 :  35;
+			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + nameBackgrBon ,xBackgrBon, yBackgrBon, wBackgrBon, hBackgrBon);
+			
+			let wLuch = isMobile ? 233 : 200;
+			let xLuch = isMobile ? -20 : 185;
+			let yLuch = isMobile ? -20 :  35;
+			let luch1 = Handler.showImgRect(self.mainGroup, "luch.png",xLuch,yLuch,wLuch, wLuch);
+			luch1.alpha = 0.35;
+			let luch2 = Handler.showImgRect(self.mainGroup, "luch.png",xLuch,yLuch,wLuch, wLuch);
+			luch2.alpha = 0.35;
 			TweenMax.to( luch1 , 10 ,{ angle: 360 , repeat: -1, ease: Power0.easeNone } );	
 			TweenMax.to( luch2 , 10 ,{ angle: -360 , repeat: -1, ease: Power0.easeNone });
 			
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "bon1AcSale1.png",185,35,135,150);
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "bon2AcSale1.png",125,-45,60,61);
-			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "bon3AcSale1.png",125,115,63,63);
+			let xBons    = [ null, 185, 125, 125 ];
+			let yBons    = [ null,  35, -45, 115 ];
+			let xBonsMob = [ null, -10, -120, 124 ];
+			let yBonsMob = [ null, -26,   37, -93 ];
+			for (let i = 1; i <= 3; i++ ) {
+				let endName = isMobile ? "AcSale1Mob.png" :  "AcSale1.png";
+				let xBon = isMobile ?  xBonsMob[i] : xBons[i];
+				let yBon = isMobile ?  yBonsMob[i] : yBons[i];
+				let bon = Handler.showImg(self.mainGroup, Consts.DIR_AC_SALE1 + 'bon'+i+endName, xBon, yBon );
+				if ( !isMobile ) {
+					bon.width /= 2;
+					bon.height /= 2;
+				}
+			};
+			//LableInfo
+			let nameLableInfo = isMobile ? "lableBonAcSale1Mob.png" : "lableBonAcSale1.png";
+			let xLableInfo = isMobile ?   0 : -90;
+			let yLableInfo = isMobile ? 162 :  10;
+			let wLableInfo = isMobile ? 355 : 354;
+			let hLableInfo = isMobile ? 110 : 142;
+			Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + nameLableInfo, xLableInfo, yLableInfo, wLableInfo, hLableInfo);
+			//butBuy
+			let xBut = isMobile ?    0 : -80;
+			let yBut = isMobile ?  260 : 125;
+			let but = Handler.showImgRect(self.mainGroup, Consts.DIR_AC_SALE1 + "butBuyAcSale1.png",xBut,yBut,281,69);
+			let onBut = function(evt) {
+				console.log("bonBuy");
+			};
+			but.on("pointertap", onBut);
 		};
 		if ( Handler.windsWithLoadedImages[ Winds.WIND_ACTION1 ] == null ) {
 			Handler.windsWithLoadedImages[ Winds.WIND_ACTION1 ] = 1;
-				let listOfImages = [
-					"winds/acSale1/backgrAcSale1.png",
-					"winds/acSale1/lableAcSale1.png",
-					"winds/acSale1/butBuyAcSale1.png",
-					"winds/acSale1/lableBonAcSale1.png",
-					"winds/acSale1/backgrAnimAcSale1.png",
-					"winds/acSale1/bon1AcSale1.png",
-					"winds/acSale1/bon2AcSale1.png",
-					"winds/acSale1/bon3AcSale1.png",
-				];
+			let listOfImages = [];
+				if ( isMobile ) { 
+					listOfImages = [
+						"winds/acSale1/backgrAcSale1Mob.png",
+						"winds/acSale1/lableAcSale1Mob.png",
+						"winds/acSale1/butBuyAcSale1.png",
+						"winds/acSale1/lableBonAcSale1Mob.png",
+						"winds/acSale1/backgrAnimAcSale1Mob.png",
+						"winds/acSale1/bon1AcSale1Mob.png",
+						"winds/acSale1/bon2AcSale1Mob.png",
+						"winds/acSale1/bon3AcSale1Mob.png",
+					];
+				} else { 
+					listOfImages = [
+						"winds/acSale1/backgrAcSale1.png",
+						"winds/acSale1/lableAcSale1.png",
+						"winds/acSale1/butBuyAcSale1.png",
+						"winds/acSale1/lableBonAcSale1.png",
+						"winds/acSale1/backgrAnimAcSale1.png",
+						"winds/acSale1/bon1AcSale1.png",
+						"winds/acSale1/bon2AcSale1.png",
+						"winds/acSale1/bon3AcSale1.png",
+					];
+				}
 				ImageLoader.loadAssets(showContent, listOfImages);
 		} else {
 			showContent();
 		};
-		return self.mainGroup();
+		return self.mainGroup;
 	};
 	
-	CWindAcSale1.shutdown = function(){
+	CWindAction1.shutdown = function(){
 		if ( this.windIndex != null ) {
 		    Winds.shutdown( this.windIndex )
 		}
         Handler.removeWindAfterTransition( this.mainGroup );
     };
 
-	//return CWindAcSale1;
+	//return CWindAction1;
