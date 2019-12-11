56ed315045451a69e95c9d983c71f1089bd50cb9
MayantsevIvan <31386901+MayantsevIvan@users.noreply.github.com>
2019-12-11 14:35:35 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -32,11 +32,11 @@
 						}
 						User.init( response );//User must inited before Winds.WIND_MENU_LEVELS
 						Winds.show( Winds.WIND_MENU_LEVELS );
-						//Winds.show( Winds.WIND_FR_INVITED );
-						//Winds.show( Winds.WIND_ACTION0 );
-						//Winds.show( Winds.WIND_ACTION1 );
-						//Winds.show( Winds.WIND_DAILY_BONUS, { num_day: 1 } );
-						//Winds.show( Winds.WIND_END_LEVEL );
+						Winds.show( Winds.WIND_FR_INVITED );
+						Winds.show( Winds.WIND_ACTION0 );
+						Winds.show( Winds.WIND_ACTION1 );
+						Winds.show( Winds.WIND_DAILY_BONUS, { num_day: 1 } );
+						Winds.show( Winds.WIND_END_LEVEL );
 						Winds.show( Winds.WIND_PUZZLE );
 						Head.init( response );
 						if (parseInt(response['db'])>0) Winds.show( Winds.WIND_DAILY_BONUS, { num_day: parseInt(response.num_day) } );			
