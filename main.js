b477712ca973ca1d7188c91b96dd07b94edc1d9d
MayantsevIvan <31386901+MayantsevIvan@users.noreply.github.com>
2019-12-20 17:43:09 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,9 @@
 						//Winds.show( Winds.WIND_ACT_INVITE );
 						//Winds.show( Winds.WIND_FR_INVITED );
 						//Winds.show( Winds.WIND_ACTION0 );
-						Winds.show( Winds.WIND_ACTION1 );
+						//Winds.show( Winds.WIND_ACTION1 );
+						Winds.show( Winds.WIND_FR_INVITED );
+						//Winds.show( Winds.WIND_ACT_INVITE );
 						//Winds.show( Winds.WIND_DAILY_BONUS, { num_day: 1 } );
 						//Winds.show( Winds.WIND_END_LEVEL );
 						//Winds.show( Winds.WIND_PUZZLE );
